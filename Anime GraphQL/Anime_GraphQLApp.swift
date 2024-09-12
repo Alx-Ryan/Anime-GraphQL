b23897c5e02deb6478b94ca7c74e1524e3cd342e
Alex <161768744+Alx-Ryan@users.noreply.github.com>
2024-09-12 17:47:21 -0500

    Initial Commit

---
+++ Anime GraphQL/Anime_GraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anime_GraphQLApp.swift
+//  Anime GraphQL
+//
+//  Created by Alex Ryan on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Anime_GraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
